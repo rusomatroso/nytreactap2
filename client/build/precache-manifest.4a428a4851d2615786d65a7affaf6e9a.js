74b175f65c2bc59b67c51181fa9b108f926b01eb
rusomatroso <evgeny23kl@gmail.com>
2019-02-18 13:47:36 -0500

    build added

---
+++ client/build/precache-manifest.4a428a4851d2615786d65a7affaf6e9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e98e4bd6278a073c1e09",
+    "url": "/static/js/main.e98e4bd6.chunk.js"
+  },
+  {
+    "revision": "cc87cb52193cabdefdca",
+    "url": "/static/js/1.cc87cb52.chunk.js"
+  },
+  {
+    "revision": "e98e4bd6278a073c1e09",
+    "url": "/static/css/main.c3b21431.chunk.css"
+  },
+  {
+    "revision": "5262760969864034ddc3a2c4c98a46d9",
+    "url": "/index.html"
+  }
+];
